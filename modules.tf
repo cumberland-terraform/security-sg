4fb1b8f6bdc53e54f1abf7ab2b9b5aca0ce1acd0
chinchalinchin <chinchalinchin@gmail.com>
2024-12-06 11:31:33 -0500

    turn off sg queries

--- modules.tf
+++ modules.tf
@@ -6,8 +6,8 @@ module "platform" {
   hydration             = {
     vpc_query           = true
     subnets_query       = false
-    dmem_sg_query       = false
-    rhel_sg_query       = false
+    public_sg_query     = false
+    private_sg_query    = false
     eks_ami_query       = false
     acm_cert_query      = false
   }
