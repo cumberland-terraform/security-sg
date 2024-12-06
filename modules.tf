cebd99bf9333f3818d96cf6326e77aca42b37c89
chinchalinchin <chinchalinchin@gmail.com>
2024-12-06 13:05:27 -0500

    update module queries

--- modules.tf
+++ modules.tf
@@ -5,10 +5,5 @@ module "platform" {
   
   hydration             = {
     vpc_query           = true
-    subnets_query       = false
-    public_sg_query     = false
-    private_sg_query    = false
-    eks_ami_query       = false
-    acm_cert_query      = false
   }
 }
